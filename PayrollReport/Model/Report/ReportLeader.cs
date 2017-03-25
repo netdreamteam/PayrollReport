6eeb1a83efce0ea294d9d6e1f90ede131d0baeed
kun <kun2009@foxmail.com>
2017-03-25 16:57:25 +0800

    增加了Report实体

---
+++ PayrollReport/Model/Report/ReportLeader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Report
+{
+    /// <summary>
+    /// 领导岗位工资报表
+    /// </summary>
+    public class ReportLeader:PayCost
+    {
+
+        /// <summary>
+        /// 岗位名称
+        /// </summary>
+        public string PositionName { get; set; }
+        /// <summary>
+        /// 职级名称
+        /// </summary>
+        public string PostRankName { get; set; }
+        /// <summary>
+        /// 实际发放工资人数
+        /// </summary>
+        public int AlreadyCount { get; set; }
+        /// <summary>
+        /// 未发工资人数
+        /// </summary>
+        public int NotCount { get; set; }
+        /// <summary>
+        /// 补发工资人数
+        /// </summary>
+        public int ReuseCount { get; set; }
+        /// <summary>
+        /// 未发工资名单
+        /// </summary>
+        public string NotName { get; set; }
+
+        /// <summary>
+        /// 补发工资名单
+        /// </summary>
+        public string ReuseName { get; set; }
+    }
+}
