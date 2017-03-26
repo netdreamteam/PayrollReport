8ff8e7c8dc3b9916adb2b8335d1d99a2bdc08e82
王姣 <1447222955@qq.com>
2017-03-26 14:23:10 +0800

    王姣——添加数据控制器模块

---
+++ PayrollReport/Controller/AddSourceInfoController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    /// <summary>
+    /// 添加从excel中获取的数据到数据库中
+    /// </summary>
+    public class AddSourceInfoController
+    {
+    }
+}
