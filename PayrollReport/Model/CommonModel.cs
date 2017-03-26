edfe586f559add8269f942455dbd5cfbd9e35d99
王姣 <1447222955@qq.com>
2017-03-26 15:47:57 +0800

    王姣—— 读取信息到数据库中

---
+++ PayrollReport/Model/CommonModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// 公共变量类
+    /// </summary>
+    public class CommonModel
+    {
+        public static string DataSourceSheetName = "源数据";
+    }
+}
