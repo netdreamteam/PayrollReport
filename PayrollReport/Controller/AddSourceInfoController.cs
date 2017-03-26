ada6bdef3b67d4f20e57cb2efccd9dc6a63615bd
王姣 <1447222955@qq.com>
2017-03-26 18:35:43 +0800

    王姣

--- PayrollReport/Controller/AddSourceInfoController.cs
+++ PayrollReport/Controller/AddSourceInfoController.cs
@@ -1,11 +1,8 @@
-﻿using Model;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Transactions;
 
 namespace Controller
 {
@@ -234,5 +231,6 @@ namespace Controller
 
             return id;
         }
+
     }
 }
