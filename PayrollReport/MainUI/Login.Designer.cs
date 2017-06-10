971b6934d82d8e4e64fede055557b4503ae5da17
jiangbo <973701800@qq.com>
2017-06-10 22:46:42 +0800

    sdf

--- PayrollReport/MainUI/Login.Designer.cs
+++ PayrollReport/MainUI/Login.Designer.cs
@@ -90,10 +90,13 @@
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.button1.Location = new System.Drawing.Point(80, 163);
+            this.button1.Location = new System.Drawing.Point(100, 163);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.Size = new System.Drawing.Size(100, 23);
             this.button1.TabIndex = 5;
             this.button1.Text = "登陆";
             this.button1.UseVisualStyleBackColor = true;
@@ -116,7 +119,7 @@
             this.MinimizeBox = false;
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "工资管理系统";
+            this.Text = "柳州高速工资系统";
             this.Load += new System.EventHandler(this.Login_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
